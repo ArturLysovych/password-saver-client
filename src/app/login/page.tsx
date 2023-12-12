01027a0e5ae3d6399ef3c5f30764b6ae49e032f5
ArturLysovych <arturlisovic@gmail.com>
2023-12-12 11:27:39 +0200

    Finished making base registration and loggining

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,41 @@
+'use client'
 import Link from "next/link"
+import axios from "axios"
+import { useState } from "react"
 
 export default function LogForm() {
+    const [loginVal, setLoginVal] = useState('');
+    const [passwordVal, setPasswordVal] = useState('');
+
+    const user = { login: loginVal, password: passwordVal };
+    
+    const postUser = (user: Object) => {
+        const postData = user;
+
+        axios.post('http://localhost:3001/users/login-user', postData)
+        .then((response) => {
+            if(response.data === true) alert('Succesfully login');
+            else if (response.data === false) alert('False data');
+            else alert(response.data);
+        })
+    }
+
     return (
         <div className="w-full h-screen flex justify-center items-center bg-red-400">
-            <form className="h-auto max-w-sm bg-white border-2 flex flex-col items-center justify-between gap-3 p-8 rounded-xl">
-              <h2 className="text-center text-lg">Create account</h2>
-              <input type="text" placeholder="Login" className="register-input" />
-              <input type="password" placeholder="Password" className="register-input" />
+            <form className="h-auto max-w-sm bg-white border-2 flex flex-col items-center justify-between gap-3 p-8 rounded-xl"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    postUser(user);
+                }}
+            >              <h2 className="text-center text-lg">Login here</h2>
+              <input type="text" placeholder="Login" className="login-input" 
+                value={loginVal}
+                onChange={(e) => setLoginVal(e.target.value)}
+              />
+              <input type="password" placeholder="Password" className="login-input" 
+                value={passwordVal}
+                onChange={(e) => setPasswordVal(e.target.value)}
+              />
               <button className="w-full p-2 border-2 rounded-md">Sign in</button>
               <p className="text-sm mt-6">Don't have an account yet? 
                 <Link href="/register">
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,46 @@
+'use client'
 import Link from "next/link"
+import axios from "axios"
+import { useState } from "react"
 
 export default function RegForm() {
+    const [loginVal, setLoginVal] = useState('');
+    const [passwordVal, setPasswordVal] = useState('');
+    const [emailVal, setEmailVal] = useState('');
+
+    const user = { login: loginVal, password: passwordVal, email: emailVal };
+
+    const postUser = (user: Object) => {
+        const postData = user;
+
+        axios.post('http://localhost:3001/users/register-user', postData)
+        .then((response) => {
+            alert(response.data);
+        })
+    }
+
     return (
         <div className="w-full h-screen flex justify-center items-center bg-red-400">
-            <form className="h-auto max-w-sm bg-white border-2 flex flex-col items-center justify-between gap-3 p-8 rounded-xl">
+            <form className="h-auto max-w-sm bg-white border-2 flex flex-col items-center justify-between gap-3 p-8 rounded-xl"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    postUser(user);
+                }}
+            >
               <h2 className="text-center text-lg">Create account</h2>
-              <input type="text" placeholder="Login" className="register-input" />
-              <input type="password" placeholder="Password" className="register-input" />
-              <input type="email" placeholder="Email" className="register-input" />
-              <button className="w-full p-2 border-2 rounded-md">Sign up</button>
+              <input type="text" placeholder="Login" className="register-input" 
+                value={loginVal}
+                onChange={(e) => setLoginVal(e.target.value)}
+              />
+              <input type="password" placeholder="Password" className="register-input" 
+                value={passwordVal}
+                onChange={(e) => setPasswordVal(e.target.value)}
+              />
+              <input type="email" placeholder="Email" className="register-input" 
+                value={emailVal}
+                onChange={(e) => setEmailVal(e.target.value)}
+              />
+              <button type="submit" className="w-full p-2 border-2 rounded-md">Sign up</button>
               <p className="text-sm mt-6">Have already an account?
                 <Link href="/login">
                   <span className="underline cursor-pointer ml-1">Login here</span>
