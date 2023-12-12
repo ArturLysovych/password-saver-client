eb20a95788ceeba93fd56b8d70cef3950a35f4d9
ArturLysovych <arturlisovic@gmail.com>
2023-12-12 14:36:27 +0200

    fixed mini-bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,9 @@
-c
+export default function Main() {
+    return (
+      <main className="h-screen w-full flex items-center justify-center flex-col">
+        <h2 className="text-5xl text-red-400">Loader page</h2>
+        <p className="text-gray-400 text-lg mt-4">that will be loader here ...</p>
+      </main>
+    );
+  }
+  
